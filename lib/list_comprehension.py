9f9818a206729a5e8aa46fa8a83a447507705af4
Brian Ashman <brian.ashman@comcast.net>
2023-07-10 10:14:02 -0700

    Complete List Comprehensions Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if n % 2 == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence_list = [f"{str}!" for str in (sentence_list)]
+    return exclamation_sentence_list
